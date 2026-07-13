563e1a20b2ec087287e6a91d79581495f8f661a1
Nevin Babu <nevinbabu25@gmail.com>
2026-07-13 23:13:55 +0530

    initial-commit

---
+++ ingestion-service/src/main/java/com/nev/ingestion_service/controller/IngestionController.java
@@ -0,0 +1,20 @@
+package com.nev.ingestion_service.controller;
+
+import com.nev.ingestion_service.dto.EnergyUsageDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/ingestion")
+public class IngestionController {
+    private final IngestionService ingestionService;
+
+    public IngestionController(IngestionService ingestionService){
+        this.ingestionService = ingestionService;
+    }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void ingestData(@RequestBody EnergyUsageDto usageDto){
+        ingestionService.ingestEnergyUsage(usageDto);
+    }
+}

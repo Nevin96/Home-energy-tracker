273b04a7fe2be66d44a8e3fcd6239be51ee9e644
Nevin Babu <nevinbabu25@gmail.com>
2026-07-14 16:53:27 +0530

    kafka-worked-successfully

--- ingestion-service/src/main/java/com/nev/ingestion_service/service/IngestionService.java
+++ ingestion-service/src/main/java/com/nev/ingestion_service/service/IngestionService.java
@@ -1,5 +1,6 @@
 package com.nev.ingestion_service.service;
 
+import com.nev.ingestion_service.dto.EnergyUsageDto;
 import com.nev.ingestion_service.kafka.event.EnergyUsageEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -11,5 +12,14 @@ public class IngestionService {
     private final KafkaTemplate<String, EnergyUsageEvent> kafkaTemplate;
     public IngestionService(KafkaTemplate<String, EnergyUsageEvent> kafkaTemplate){
         this.kafkaTemplate = kafkaTemplate;
+    }
+    public void ingestEnergyUsage(EnergyUsageDto input){
+        EnergyUsageEvent event = EnergyUsageEvent.builder()
+                .deviceId(input.deviceId())
+                .energyConsumed(input.energyConsumed())
+                .timeStamp(input.timeStamp())
+                .build();
+        kafkaTemplate.send("energy-usage",event);
+        log.info("Ingested energy usage: {}",event);
     }
 }
